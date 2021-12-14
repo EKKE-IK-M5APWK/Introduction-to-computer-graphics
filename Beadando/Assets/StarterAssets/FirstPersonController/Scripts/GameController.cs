b36c171f15036bcc230393614e66e4e0db8141f7
Milán Kormány <kormany.1993@gmail.com>
2021-12-14 01:52:42 +0100

    Adding Ray Casting and a box collider around the map

--- Beadando/Assets/StarterAssets/FirstPersonController/Scripts/GameController.cs
+++ Beadando/Assets/StarterAssets/FirstPersonController/Scripts/GameController.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 using Cinemachine;
 using StarterAssets;
 
-
 public class GameController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera zoomVirtualCamera;
     [SerializeField] private float baseSesnsitivity;
     [SerializeField] private float zoomSensitivity;
+    [SerializeField] private LayerMask targetColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform testObjectTransform;
     private FirstPersonController firstPersonController;
     private StarterAssetsInputs starterAssetsInputs;
 
@@ -24,11 +25,24 @@ public class GameController : MonoBehaviour
         {
             zoomVirtualCamera.gameObject.SetActive(true);
             firstPersonController.SetSensitivity(zoomSensitivity);
+
         }
         else
         {
             zoomVirtualCamera.gameObject.SetActive(false);
             firstPersonController.SetSensitivity(baseSesnsitivity);
         }
+
+        Vector2 monitorCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Debug.Log(monitorCenter);
+        Ray ray = Camera.main.ScreenPointToRay(monitorCenter);
+        if (Physics.Raycast(ray, out RaycastHit target, Mathf.Infinity, targetColliderLayerMask))
+        {
+            testObjectTransform.position = target.point;
+
+            Debug.DrawLine(Camera.main.transform.position, target.point, Color.cyan, 0.5f);
+        }
+
+
     }
 }
